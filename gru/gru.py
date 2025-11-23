f78917224d3c3ebf7df86f8f12985b5587c859bb
Dhrimanti <dhritimancg@gmail.com>
2025-11-23 20:38:03 +0530

    Completing

--- gru/gru.py
+++ gru/gru.py
@@ -18,4 +18,30 @@ testX = np.float32(testX) / 255
 
 
 class GRUCell(nn.Module):
-    def __init__(self,input_size,hidden_size,)
+    def __init__(self,input_size,hidden_size,bias=True,device=None):
+        super(GRUCell,self).__init__()
+        self.input_size=input_size
+        self.hidden_size=hidden_size
+        self.bias=bias
+
+        self.W_z=nn.Parameter(torch.Tensor(hidden_size,input_size))
+        self.W_r=nn.Parameter(torch.Tensor(hidden_size,input_size))
+        self.W_h=nn.Parameter(torch.Tensor(hidden_size,input_size))
+
+
+        self.U_z=nn.Parameter(torch.Tensor(hidden_size,hidden_size))
+        self.U_r=nn.Parameter(torch.Tensor(hidden_size,hidden_size))
+        self.U_h=nn.Parameter(torch.Tensor(hidden_size,hidden_size))
+
+        if bias:
+            self.b_z=nn.Parameter(torch.Tensor(hidden_size))
+            self.b_r=nn.Parameter(torch.Tensor(hidden_size))
+            self.b_h=nn.Parameter(torch.Tensor(hidden_size))
+
+        else:
+            self.register_parameter('b_z',None)
+            self.register_parameter('b_r',None)
+            self.register_parameter('b_h',None) 
+
+        
+
