20f04ef2ce41aa669c01ec46dc8a3f59de813482
Dhrimanti <dhritimancg@gmail.com>
2025-11-23 21:22:34 +0530

    Pls

--- gru/gru.py
+++ gru/gru.py
@@ -8,7 +8,10 @@ import tqdm as tqdm
 import seaborn as sns
 import matplotlib.pyplot as plt
 from keras.datasets.mnist import load_data
-
+from torchtext.datasets import IMDB
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch.utils.data import DataLoader
 
 sns.set_theme()
 
@@ -16,6 +19,42 @@ sns.set_theme()
 trainX = np.float32(trainX) / 255
 testX = np.float32(testX) / 255
 
+def preprocess_imdb(batch_size=32, max_length=500):
+    tokenizer = get_tokenizer('basic_english')
+    
+    train_iter = IMDB(split='train')
+    test_iter = IMDB(split='test')
+    
+    def yield_tokens(data_iter):
+        for _, text in data_iter:
+            yield tokenizer(text)
+    
+    vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=['<unk>', '<pad>'])
+    vocab.set_default_index(vocab['<unk>'])
+    
+    text_pipeline = lambda x: [vocab[token] for token in tokenizer(x)]
+    label_pipeline = lambda x: 1 if x == 'pos' else 0
+    
+    def collate_batch(batch):
+        text_list, label_list = [], []
+        for (_label, _text) in batch:
+            processed_text = text_pipeline(_text)
+            if len(processed_text) > max_length:
+                processed_text = processed_text[:max_length]
+            text_list.append(torch.tensor(processed_text, dtype=torch.long))
+            label_list.append(label_pipeline(_label))
+        
+        text_list = torch.nn.utils.rnn.pad_sequence(text_list, batch_first=True, padding_value=vocab['<pad>'])
+        label_list = torch.tensor(label_list, dtype=torch.long)
+        
+        return text_list, label_list
+    
+    train_loader = DataLoader(list(train_iter), batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
+    test_loader = DataLoader(list(test_iter), batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
+    
+    return train_loader, test_loader, vocab, len(vocab)
+
+train_loader, test_loader, vocab, vocab_size = preprocess_imdb(batch_size=32)
 
 class GRUCell(nn.Module):
     def __init__(self,input_size,hidden_size,bias=True,device=None):
@@ -59,4 +98,10 @@ class GRUCell(nn.Module):
         return h_last
 
 
+def train(model,optimizer,loss_fct=torch.nn.NLLLoss(),nb_epochs=500,batch_size=128):
+    model.train()
+    for i in range(nb_epochs):
+        total_loss=0
+        for batch_idx,(data,target) in enumerate(train_loader):
+            optimizer.zero_grad()
 
