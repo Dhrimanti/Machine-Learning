28a5f2bad221b3b3892f1e6eb3501abaf1913610
Dhrimanti <dhritimancg@gmail.com>
2025-12-04 22:56:29 +0530

    Please help me out

--- freeflow/freeflow.py
+++ freeflow/freeflow.py
@@ -23,3 +23,29 @@ class Dataset(torch.utils.data.Dataset):
         assert self.dist1.shape==self.dist2.shape
 
     def __len__(self):
+        return self.dist1.shape[0]
+    def __getitem__(self,idx):
+        return self.dist1[idx],self.dist2[idx]
+    
+def sample_multimodal_distribution(modes, std, batch_size=1000):
+    dataset = []
+    for i in range(batch_size):
+        sample = np.random.randn(modes.shape[1]) * std
+        mode_idx = np.random.randint(modes.shape[0])
+        sample[0] += modes[mode_idx, 0]
+        sample[1] += modes[mode_idx, 1]
+        dataset.append(sample)
+    return np.array(dataset, dtype="float32")
+
+
+def train_rectified(rectified_flow,optimizer,train_loader,NB_EPC,eps=1e-15):
+    for i in tqdm(range(NB_EPC)):
+        for z0,z1 in (train_loader):
+            t=torch.rand((z1.shape[0],1))
+            z_t=t*z1+(1.-t)*z0
+            target=z1-z0
+            pred=rectified_flow(z_t,t)
+            loss=(target-pred).view(pred.shape[0],-1).abs().pow(2).sum(dim=1).mean()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
