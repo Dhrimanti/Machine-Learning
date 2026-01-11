22d38d73f0ec72e5fcb93dd6e91d8c8f1cdba26c
Dhrimanti <dhritimancg@gmail.com>
2026-01-11 10:34:34 +0530

    I am dying

--- gan.py
+++ gan.py
@@ -1,3 +1,10 @@
 import torch 
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import tqdm as tqdm
+from matplotlib import pyplot as plt
+from keras.datasets.mnist import load_data  
+
+(trainX, trainy), (testX, testy) = load_data()
+trainX=(np.float32(trainX)-127.5)/127.5
