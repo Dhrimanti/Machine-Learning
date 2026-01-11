a8c44901388bb149fe6344e5f4bc4e64552308f5
Dhrimanti <dhritimancg@gmail.com>
2026-01-11 10:57:44 +0530

    I am dying

--- gan.py
+++ gan.py
@@ -8,3 +8,15 @@ from keras.datasets.mnist import load_data
 
 (trainX, trainy), (testX, testy) = load_data()
 trainX=(np.float32(trainX)-127.5)/127.5
+
+
+def get_minibatch(batch_size):
+    indices=torch.randperm(trainX.shape[0])[:batch_size]
+    return torch.tensor(trainX[indices],dtype=torch.float).reshape(batch_size,-1)
+
+def sample_noise(size,dim=100):
+    out=torch.empty(size,dim)
+    mean=torch.zeros(size,dim)
+    std=torch.ones(dim)
+    torch.normal(mean,std,out=out)
+    return out
