8f6a6987726ae9416221e47293567a988d9cf704
Dhrimanti <dhritimancg@gmail.com>
2026-01-11 17:41:17 +0530

    I am dying

--- gan.py
+++ gan.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import tqdm as tqdm
+from tqdm import tqdm
 from matplotlib import pyplot as plt
 from keras.datasets.mnist import load_data  
 
